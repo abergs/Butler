8cca34512b85fe14140cff8abdb00c63d964d546
Anders Åberg <abergs.anders@gmail.com>
2013-02-02 23:38:12 +0100

    Initial commit

---
+++ Buttler/Models/Page.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Buttler.Models
+{
+    public class Page : Unit
+    {
+        public string Title { get; set; }
+    }
+}
---
+++ Buttler/Models/Unit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Buttler.Models
+{
+    public class Unit : IUnit
+    {
+        public string Content { get; set; }
+    }
+
+    interface IUnit {
+        public string Content { get; set; }
+    }
+}
