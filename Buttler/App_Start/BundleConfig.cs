483034417dfa999405ed2844b220b93f69247fe8
Anders Åberg <abergs.anders@gmail.com>
2013-02-06 21:50:25 +0100

    Wrapped up semi done TemplateRenderin and added Optimizations

---
+++ Buttler/App_Start/BundleConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Buttler.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("ButtlerWithStyle").Include(
+                "~/Content/themes/base/main.css"));
+        }
+    }
+}
--- Buttler/Models/RenderService.cs
+++ Buttler/Models/RenderService.cs
@@ -2,36 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Buttler.Models
 {
-    public class RenderService
+    public sealed class RenderService
     {
-        public sealed class DataService
+        private static readonly RenderService instance = new RenderService();
+        private ButtlerContext context;
+
+        private RenderService()
         {
-            private static readonly DataService instance = new DataService();
-            private ButtlerContext context;
+            context = new ButtlerContext();
+        }
 
-            private DataService()
+        public static RenderService Instance
+        {
+            get
             {
-                context = new ButtlerContext();
-            }
-
-            public static DataService Instance
-            {
-                get
-                {
-                    return instance;
-                }
-            }
-
-            public static dynamic Get(string id)
-            {
-                // How should we structure data?
-                // One table with ID / type ?
-                // EF would then lookup ID and Type in that table and join the TypeTable for that ID ??
-                return instance.context.Units.FirstOrDefault(p => p.Name == id);
+                return instance;
             }
         }
+
+        public static string Get(string id)
+        {
+            // How should we structure data?
+            // One table with ID / type ?
+            // EF would then lookup ID and Type in that table and join the TypeTable for that ID ??
+            dynamic data =  DataService.Get(id);
+            var template = ""; // Read from file or maybe DB. Use IoC?
+            
+            string result = RazorEngine.Razor.Parse(template,data);
+            return result;           
+        }
     }
+
 }
